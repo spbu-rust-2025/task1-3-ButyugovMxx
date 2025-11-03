aab586bc66ca5b8249a7a74bb0b6340b97423e36
Maxim Butyugov <dmitrijbutugov85436@gmail.com>
2025-11-04 00:17:08 +0300

    провека на открытие файла и чтение из него

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
+use std::io::{self, Read};
+use std::fs::File;
 
+fn main() {
+    let mut s = String::new();
+    if io::stdin().read_line(&mut s).is_err(){
+        println!("failure");
+        return;
+    }
+
+    let path = s.trim();
+
+    let res = File::open(path);
+
+    let mut file = match res{
+        Ok(f) => f,
+        Err(_) => {
+            println!("failure");
+            return;
+        }
+    };
+
+    let mut contents = String::new();
+    if file.read_to_string(&mut contents).is_ok(){
+        println!("success");
+    }else{
+        println!("failure");
+    }
 }
